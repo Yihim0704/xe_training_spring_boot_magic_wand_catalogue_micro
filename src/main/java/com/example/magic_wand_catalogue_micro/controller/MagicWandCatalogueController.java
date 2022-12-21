a4a3cc613a1e8f564ef84154ba9d89e3fbaeec6f
yihim <yihim.chan@accenture.com>
2022-12-21 21:25:43 +0800

    modified request mapping url

--- src/main/java/com/example/magic_wand_catalogue_micro/controller/MagicWandCatalogueController.java
+++ src/main/java/com/example/magic_wand_catalogue_micro/controller/MagicWandCatalogueController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/magic-wand-catalogue")
+@RequestMapping("/api/v1/magic-wand-catalogue")
 public class MagicWandCatalogueController {
 
     @Autowired
